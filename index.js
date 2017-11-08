f554e7bd0d56a7d7c7331f58571a1ad71c93caf4
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2017-11-08 22:14:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(blocks) {
-  return var distance = blocks - 42;
+  var distance = blocks - 42;
 
-  if (distance < 0) {
-    return distance 
-  }
+  distance > 0 ? 
 }

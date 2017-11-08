e4b2e5c1d4598abec808c85793033a1f0ea86998
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2017-11-08 22:21:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ function distanceFromHqInFeet(blocks) {
   return feet = distanceFromHqInBlocks(blocks) * 264;
 }
 
-function 
+function distanceTravelledInFeet(start, end) {
+  return distance = Math.abs(start - end) * 264;
+}

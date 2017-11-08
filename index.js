a6e68d609d78bd240b37e32f7134c42319b0fe7e
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2017-11-08 22:17:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(blocks) {
-  return let blocks =  Math.abs(blocks - 42);
+  return blocks =  Math.abs(blocks - 42);
 }
-// 
+//
 // function distanceFromHqInFeet(blocks) {
 //   return let feet = distanceFromHqInBlocks(blocks) * 264;
 // }
